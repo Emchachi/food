7ff71f52793699bc40c3e4660f27fcde15cc1468
emchachi <emiliyanhristov99@gmail.com>
2022-03-21 16:28:03 +0200

    upload

--- script.js
+++ script.js
@@ -6,9 +6,32 @@ const circleBig = document.querySelector(".circle-1");
 const bg2 = document.querySelector(".second-half");
 
 btnNavEl.addEventListener("click", function (e) {
+  btnNavEl.classList.toggle("nav-open");
   bg1.classList.toggle("nav-open");
   bg2.classList.toggle("nav-open");
   nav.classList.toggle("nav-open");
   circle.classList.toggle("nav-open");
   circleBig.classList.toggle("nav-open");
 });
+
+// Drawing
+let path = document.querySelector("path");
+let pathLength = path.getTotalLength();
+
+path.style.strokeDasharray = pathLength + " " + pathLength;
+
+path.style.strokeDashoffset = pathLength;
+
+window.addEventListener("scroll", () => {
+  let scrollPercentage =
+    (document.documentElement.scrollTop + document.body.scrollTop) /
+    (document.documentElement.scrollHeight -
+      document.documentElement.clientHeight);
+  console.log(scrollPercentage);
+
+  let drawLength = pathLength * scrollPercentage;
+
+  path.style.strokeDashoffset = pathLength - drawLength;
+});
+
+// Reveal
